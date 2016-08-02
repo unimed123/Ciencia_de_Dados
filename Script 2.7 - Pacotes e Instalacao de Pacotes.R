bdcdbbd6c0dbd0fea848bf1b40145ea904ce0063
Fernando Carvalho <fernandojllesp@gmail.com>
2016-08-02 20:27:10 -0300

    Add files via upload

---
+++ Script 2.7 - Pacotes e Instalacao de Pacotes.R
@@ -0,0 +1,58 @@
+###############################################################################
+# CURSO:  Formação Cientista de Dados        PLATAFORMA: Data Science Academy #
+# MÓDULO: Big Data Analytics com R           INSTRUTOR:  Daniel Mendes        #
+# Data: 01/08/2016 - inicio aula: 9:13pm     fim aula: h:mmpm                                 #
+###############################################################################
+
+# Pacotes e Instalacao de Pacotes
+
+# De onde vem as funcoes? Pacotes (conjunto de funcoes)
+# Quando voce inicia o RStudio, alguns pacotes sao carregado por padrao
+
+search()
+
+install.packages(c('ggvis', 'tm','dplyr'))
+library(ggvis)
+library(tm)
+require(dplyr)
+
+search()
+?require
+detach(package:dplyr)
+?attach
+
+ls(pos = 'package:tm')
+ls(getNamespace('tm'), all.names = TRUE)
+
+lsf.str('package:tm')
+lsf.str('package:ggplot2')
+library(ggplot2)
+lsf.str('package:ggplot2')
+
+## R possui um conjunto de datasets preinstalados. Verificar datasets package
+
+library(MASS)
+data()
+
+?lynx
+head(lynx)
+head(iris)
+tail(lynx)
+summary(lynx)
+
+plot(lynx)
+hist(lynx)
+head(iris)
+iris$Sepal.Length
+sum(Sepal.Length)
+
+attach(iris)
+sum(Sepal.Length)
+
+
+
+
+
+
+
+
