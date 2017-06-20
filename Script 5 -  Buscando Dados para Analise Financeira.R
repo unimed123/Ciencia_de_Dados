38607bc2057a8f0b9fd97b1fd755488c72a4a79a
Fernando Carvalho <fernandojllesp@gmail.com>
2017-06-19 21:11:12 -0300

    Add files via upload

---
+++ Script 5 -  Buscando Dados para Analise Financeira.R
@@ -0,0 +1,5 @@
+###############################################################################
+# CURSO:  Formação Cientista de Dados        PLATAFORMA: Data Science Academy #
+# MÓDULO: Big Data Analytics com R           INSTRUTOR:  Daniel Mendes        #
+# Data: 19/06/2017 - inicio aula: 06:29 pm     dd/mm/2016 - fim aula::pm      #
+###############################################################################
